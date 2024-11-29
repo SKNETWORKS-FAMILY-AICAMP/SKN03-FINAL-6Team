d573de717967cb6a3014cccf82d373049cc4e757
junghl11 <junghl11@naver.com>
2024-11-29 21:13:47 +0900

    change folder

--- TailorLink_Back/src/main/java/com/example/FinalPJ/board/controller/BoardController.java
+++ TailorLink_Back/src/main/java/com/example/FinalPJ/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.FinalPJ.board.controller;
 
 import org.springframework.ui.Model;
+import com.example.FinalPJ.board.dto.BoardDTO; 
 import com.example.FinalPJ.board.controller.entity.Board;
 import com.example.FinalPJ.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class BoardController {
