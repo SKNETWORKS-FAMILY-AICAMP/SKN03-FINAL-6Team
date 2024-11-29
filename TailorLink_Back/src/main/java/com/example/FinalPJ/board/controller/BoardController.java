c5705bad4c4b0a5dcd44e30c36b9399ee0e7b9e1
junghl11 <junghl11@naver.com>
2024-11-29 12:11:35 +0900

    게시판 내용 추가

--- TailorLink_Back/src/main/java/com/example/FinalPJ/board/controller/BoardController.java
+++ TailorLink_Back/src/main/java/com/example/FinalPJ/board/controller/BoardController.java
@@ -20,23 +20,24 @@ public class BoardController {
         return "boardwrite";
     }
 
-    @PostMapping("/board/writepro")
-    public String boardWritePro(Board board) {
-
-        boardService.write(board);
-        return "redirect:/board/list";
+    @PostMapping("board/writepro")
+    public ResponseEntity<?> boardWritePro(@Valid @RequestBody BoardDTO boardDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // Validation 에러 메시지 반환
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+        boardService.boardWrite(boardDTO);
+        return ResponseEntity.ok("Success");
     }
 
     @GetMapping("/board/list")
     public String boardList(Model model) {
-
         model.addAttribute("list", boardService.boardList());
         return "boardlist";
     }
 
     @GetMapping("/board/view")
     public String boardView(Model model, @RequestParam("board_id") Integer board_id) {
-
         model.addAttribute("board", boardService.boardView(board_id));
         return "boardview";
     }
@@ -44,28 +45,30 @@ public class BoardController {
     @GetMapping("/board/delete")
     public String boardDelete(@RequestParam("board_id") Integer board_id) {
         boardService.boardDelete(board_id);
-
         return "redirect:/board/list";
     }
 
     @GetMapping("/board/modify/{board_id}")
     public String boardModify(@PathVariable("board_id") Integer board_id, Model model) {
-
         model.addAttribute("board", boardService.boardView(board_id));
-
         return "boardmodify";
     }
 
     @PostMapping("/board/update/{board_id}")
-    public String boardUpdate(@PathVariable("board_id") Integer board_id, Board board) {
+    public String boardUpdate(@PathVariable("board_id") Integer board_id, @Valid @ModelAttribute BoardDTO boardDTO, BindingResult bindingResult) {
+        // 유효성 검사 실패 시 에러 처리
+        if (bindingResult.hasErrors()) {
+            return "boardmodify"; // 수정 페이지로 다시 이동
+        }
 
-        Board boardTemp = boardService.boardView(board_id);
+        BoardDTO boardTemp = boardService.boardView(board_id);
 
-        boardTemp.setTitle(board.getTitle());
-        boardTemp.setContent(board.getContent());
+        boardTemp.setTitle(boardDTO.getTitle());
+        boardTemp.setContent(boardDTO.getContent());
 
-        boardService.write(boardTemp);
+        boardService.boardWrite(boardTemp);
 
         return "redirect:/board/list";
     }
 }
+
