3c12eaa9d326f0ed844ffba587d02304822674d3
yongjoo0329 <park970329@gmail.com>
2024-12-17 17:08:41 +0900

    feat ChatBot Service and AwsTest

---
+++ TailorLink_Front/src/pages/Home/components/TextSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TextSection.css";
+
+const TextSection = () => {
+  return (
+    <div className="text-section">
+      <h2>차량 관리의 새로운 패러다임</h2>
+      <p>
+        AICFN은 차량 추천, 보험 안내, 차량 가이드를 통해 운전자에게
+        꼭 필요한 정보를 제공합니다.
+      </p>
+    </div>
+  );
+};
+
+export default TextSection;
