084ba57f2b6749248c08d458865a4e9064ba0f61
junghl11 <junghl11@naver.com>
2024-12-28 21:46:04 +0900

    유저 정보 수정

--- TailorLink_Back/src/main/java/com/example/FinalPJ/service/UserService.java
+++ TailorLink_Back/src/main/java/com/example/FinalPJ/service/UserService.java
@@ -1,7 +1,9 @@
 package com.example.FinalPJ.service;
 
 import com.example.FinalPJ.dto.UserDTO;
+import com.example.FinalPJ.dto.request.user.PatchUserRequestDTO;
 import com.example.FinalPJ.dto.request.user.UserRequestDTO;
+import com.example.FinalPJ.dto.response.user.PatchUserResponseDTO;
 import com.example.FinalPJ.dto.response.user.UserResponseDTO;
 import com.example.FinalPJ.entity.UserEntity;
 import org.springframework.http.ResponseEntity;
@@ -14,4 +16,5 @@ public interface UserService {
     void save(UserDTO userDTO);
     Optional<UserEntity> findUserByNameAndEmail(String name, String email);
     ResponseEntity<? super UserResponseDTO> notFoundId(UserRequestDTO dto);
+    ResponseEntity<? super PatchUserResponseDTO> patchUser(PatchUserRequestDTO dto, String userId);
 }
